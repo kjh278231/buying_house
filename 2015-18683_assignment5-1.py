8dfc42eb8bde8682f18bc181e8476a9f09216524
kjh278231 <kjh278231@snu.ac.kr>
2020-12-20 16:47:38 +0000

    final version - add paths

--- 2015-18683_assignment5-1.py
+++ 2015-18683_assignment5-1.py
@@ -44,11 +44,30 @@ def planning_a_date(n, edge_list, D):
     paths = [] 
     dist = {} 
     graph = build_graph(edge_list,n)
+    pi = [0]*n
+    for i in range(n):
+      pi[i] = [-1]*n
     ##### Please write your code down here #####
     # for lin in graph:
     #   print(lin)
+     
+    
     L = graph.copy()
     m=1
+    for i in range(n):
+      for j in range(n):
+        if L[i][j] != 0 and L[i][j] != -1:
+          pi[i][j] = i+1
+
+
+    # print("\n Initial Path matrix: ")
+    # for line in pi:
+    #   print(line)
+    # print("\n Initial Cost matrix: ")
+    # for line in L:
+    #   print(line)
+
+
     while(m<n):
       L_old = L.copy()
       W = L.copy()
@@ -59,12 +78,18 @@ def planning_a_date(n, edge_list, D):
       #   print(line)
       for j in range(n):
         for k in range(n):
-          MIN = D+1
+          MIN = L_old[j][k]
           for l in range(n):
             if L_old[j][l]!=-1 and W[l][k] != -1:
-              MIN = min(MIN, L_old[j][l]+W[l][k])
+              if MIN > L_old[j][l]+W[l][k] or (MIN == -1 and L_old[j][l]+W[l][k] <=D):
+                pi[j][k] = l+1
+                MIN = L_old[j][l]+W[l][k]
           if MIN <=D:
             L[j][k] =MIN
+      # print("\n Path matrix: ")
+      # for line in pi:
+      #   print(line)
+      # print("\n Cost matrix: ")
       # for line in L:
       #   print(line)
       m*=2
@@ -86,10 +111,20 @@ def planning_a_date(n, edge_list, D):
       if L[MAX_index][i] !=0 and L[MAX_index][i]!=-1:
         dist[str(i+1)] = L[MAX_index][i]
     best_city = MAX_index+1
-    print(dist)
     
+    for i in range(n):
+      if pi[best_city-1][i] == -1:
+        continue
+      li = []
+      li.append(i+1)
+      k = best_city-1
+      while(pi[k][i] != k+1):
+        k = pi[k][i]-1
+        li.append(k+1)
+      li.append(best_city)
+      paths.append(li[::-1])
+   
           
-      
     return best_city, paths, dist
 
 if __name__ == '__main__':
